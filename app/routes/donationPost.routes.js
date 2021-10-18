5aa1899f5ca9d9395bbea10ee25f4986ca8b0e5d
wenodh <vinodkumara3@gmail.com>
2021-10-18 10:39:57 +0530

    removed middleware

--- app/routes/donationPost.routes.js
+++ app/routes/donationPost.routes.js
@@ -3,53 +3,53 @@ const authJwt = require('../middleware/authJwt');
 const controller = require('../controllers/donationPost.controller');
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            'Access-Control-Allow-Headers',
-            'x-access-token, Origin, Content-Type, Accept'
-        );
-        next();
-    });
+    // app.use(function (req, res, next) {
+    //     res.header(
+    //         'Access-Control-Allow-Headers',
+    //         'x-access-token, Origin, Content-Type, Accept'
+    //     );
+    //     next();
+    // });
 
     //all donation posts
     app.get(
         '/api/donationPost',
-        authJwt.verifyToken,
+        // authJwt.verifyToken,
         controller.getAllDonationPost
     );
 
     //create donation post
     app.post(
         '/api/donationPost/:id',
-        authJwt.verifyToken,
+        // authJwt.verifyToken,
         controller.donationPost
     );
 
     //todo: pending
     app.get(
         '/api/getAllDonationPostByUserId/:id',
-        authJwt.verifyToken,
+        // authJwt.verifyToken,
         controller.getAllDonationPostByUserId
     );
 
     //update
     app.put(
         '/api/donationPost/:id',
-        authJwt.verifyToken,
+        // authJwt.verifyToken,
         controller.updateDonationPost
     );
 
     //delete
     app.delete(
         '/api/donationPost/:id',
-        authJwt.verifyToken,
+        // authJwt.verifyToken,
         controller.deleteDonationPost
     );
 
     //get a donation post by id
     app.get(
         '/api/donationPost/:id',
-        authJwt.verifyToken,
+        // authJwt.verifyToken,
         controller.getDonationPost
     );
 };
