28f57413bf920c11fcd835d36fd4d5e124b946fb
wenodh <vinodkumara3@gmail.com>
2021-10-21 15:39:02 +0530

    payment api added razorpay

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const path = require('path');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const {GridFsStorage} = require('multer-gridfs-storage');
+const { GridFsStorage } = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
-
+const Razorpay = require('razorpay');
 const corsOptions = {
     origin: 'http://localhost:8081',
 };
-
+const shortid = require('shortid');
 app.use(cors());
 
 // parse requests of content-type - application/json
@@ -27,174 +27,197 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 const Role = db.role;
-let mongoURI=db.url
+let mongoURI = db.url;
 let connection = () => {
     db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log('Successfully connect to MongoDB.');
-        initial();
-    })
-    .catch((err) => {
-        console.error('Connection error', err);
-        process.exit();
-    });
-}
-connection()
+        .connect(db.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log('Successfully connect to MongoDB.');
+            initial();
+        })
+        .catch((err) => {
+            console.error('Connection error', err);
+            process.exit();
+        });
+};
+connection();
 let gfs;
 const conn = mongoose.createConnection(db.url);
-conn.once("open", function () {
+conn.once('open', function () {
     gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection("uploads")
+    gfs.collection('uploads');
     // gfs.collection("photos");
 });
 
 //Create storage engine
 const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename =
+                    buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads',
+                };
+                resolve(fileInfo);
+            });
+        });
+    },
 });
 const upload = multer({ storage });
-
 
 // @route GET /
 // @desc Loads form
 app.get('/', (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      res.render('index', { files: false });
-    } else {
-      files.map(file => {
-        if (
-          file.contentType === 'image/jpeg' ||
-          file.contentType === 'image/png'
-        ) {
-          file.isImage = true;
+    gfs.files.find().toArray((err, files) => {
+        // Check if files
+        if (!files || files.length === 0) {
+            res.render('index', { files: false });
         } else {
-          file.isImage = false;
+            files.map((file) => {
+                if (
+                    file.contentType === 'image/jpeg' ||
+                    file.contentType === 'image/png'
+                ) {
+                    file.isImage = true;
+                } else {
+                    file.isImage = false;
+                }
+            });
+            res.render('index', { files: files });
         }
-      });
-      res.render('index', { files: files });
-    }
-  });
+    });
 });
 
 // @route POST /upload
 // @desc  Uploads file to DB
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ file: req.file });
-//   res.redirect('/');
+    res.json({ file: req.file });
+    //   res.redirect('/');
 });
 
 // @route GET /files
 // @desc  Display all files in JSON
 app.get('/files', (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist'
-      });
-    }
+    gfs.files.find().toArray((err, files) => {
+        // Check if files
+        if (!files || files.length === 0) {
+            return res.status(404).json({
+                err: 'No files exist',
+            });
+        }
 
-    // Files exist
-    return res.json(files);
-  });
+        // Files exist
+        return res.json(files);
+    });
 });
 
 // @route GET /files/:filename
 // @desc  Display single file object
 app.get('/files/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
-    // File exists
-    return res.json(file);
-  });
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        // Check if file
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists',
+            });
+        }
+        // File exists
+        return res.json(file);
+    });
 });
 
 // @route GET /image/:filename
 // @desc Display Image
 app.get('/image/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        // Check if file
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists',
+            });
+        }
 
-    // Check if image
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not an image'
-      });
-    }
-  });
+        // Check if image
+        if (
+            file.contentType === 'image/jpeg' ||
+            file.contentType === 'image/png'
+        ) {
+            // Read output to browser
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        } else {
+            res.status(404).json({
+                err: 'Not an image',
+            });
+        }
+    });
 });
 
 // @route DELETE /files/:id
 // @desc  Delete file
 app.delete('/files/:id', (req, res) => {
-  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
-    if (err) {
-      return res.status(404).json({ err: err });
-    }
-      res.json({
-    success:true
-})
-    // res.redirect('/');
-  });
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+        if (err) {
+            return res.status(404).json({ err: err });
+        }
+        res.json({
+            success: true,
+        });
+        // res.redirect('/');
+    });
 });
-// app.use("/file", upload);
 
-// simple route
-// app.get("/file/:filename", async (req, res) => {
-//     try {
-//         const file = await gfs.files.findOne({ filename: req.params.filename });
-//         const readStream = gfs.createReadStream(file.filename);
-//         readStream.pipe(res);
-//     } catch (error) {
-//         res.send("not found");
-        
-//     }
-// });
+var instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
-// app.delete("/file/:filename", async (req, res) => {
-//     try {
-//         await gfs.files.deleteOne({ filename: req.params.filename });
-//         res.send("success");
-//     } catch (error) {
-//         console.log(error);
-//         res.send("An error occured.");
-//     }
-// });
+app.post('/create/orderId', (req, res) => {
+    // console.log('create orderId request', req.body);
+    var options = {
+        amount: req.body.amount,
+        // amount: 50000,
+        currency: 'INR',
+        receipt: shortid.generate(),
+        payment_capture: 1,
+    };
+    instance.orders.create(options, (err, order) => {
+        console.log(order);
+        res.json(order);
+        // res.send({ orderId: order.id });
+        // res.json({
+        //     id: order.id,
+        //     currency: order.currency,
+        //     amount: order.amount,
+        // });
+    });
+});
+app.post('/api/payment/verify', (req, res) => {
+    let body =
+        req.body.response.razorpay_order_id +
+        '|' +
+        req.body.response.razorpay_payment_id;
+    let crypto = require('crypto');
+    let expectedSignature = crypto
+        .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
+        .update(body.toString())
+        .digest('hex');
+    console.log('sig received ', req.body.response.razorpay_signature);
+    console.log('sig generated ', expectedSignature);
+    let response = { signatureIsValid: 'false' };
+    if (expectedSignature === req.body.response.razorpay_signature)
+        response = { signatureIsValid: 'true' };
+    res.send(response);
+});
 app.get('/', (req, res) => {
     res.json({ message: 'server running' });
 });
