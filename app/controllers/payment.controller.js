4c9ab5faa33d4745873332f68cf7cb6b18790539
wenodh <vinodkumara3@gmail.com>
2021-10-24 19:00:20 +0530

    shortid added bug fix

--- app/controllers/payment.controller.js
+++ app/controllers/payment.controller.js
@@ -1,5 +1,5 @@
 const Razorpay = require('razorpay');
-
+const shortid = require('shortid');
 let instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
